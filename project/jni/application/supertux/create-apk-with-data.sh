2d98c49a218682800cf5f0bce7dbf07c253fee83
Sergii Pylypenko <x.pelya.x@gmail.com>
2022-01-06 03:47:42 +0200

    SuperTux: updated todo

--- project/jni/application/supertux/create-apk-with-data.sh
+++ project/jni/application/supertux/create-apk-with-data.sh
@@ -8,10 +8,12 @@ PASS=
 [ -n "$ANDROID_KEYSTORE_PASS_FILE" ] && PASS="--ks-pass file:$ANDROID_KEYSTORE_PASS_FILE"
 
 OUT=`pwd`/../../../../SuperTux-with-data.apk
+DATAZIP=`pwd`/../../../../SuperTux-data.zip
 rm -f $OUT $OUT-aligned
-cp ../../../../project/app/build/outputs/apk/release/app-release.apk $OUT || exit 1
 cd supertux/data || exit 1
-zip -r -9 $OUT * || exit 1
-zipalign 4 $OUT $OUT-aligned || exit 1
+[ -e $DATAZIP ] || zip -r -9 $DATAZIP * || exit 1
+cp -f ../../../../../../project/app/build/outputs/apk/release/app-release.apk $OUT || exit 1
+zipmerge $OUT $DATAZIP
+zipalign -p 4 $OUT $OUT-aligned || exit 1
 mv $OUT-aligned $OUT
 apksigner sign --ks $ANDROID_KEYSTORE_FILE --ks-key-alias $ANDROID_KEYSTORE_ALIAS $PASS $OUT || exit 1
