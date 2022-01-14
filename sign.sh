bbaa42c1ec246471be985b33b4f97ac26f3ec916
Sergii Pylypenko <x.pelya.x@gmail.com>
2022-01-14 23:58:44 +0200

    SDL: trying to use fancy Github secrets in CI

--- sign.sh
+++ sign.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # Set path to your Android keystore and your keystore alias here, or put them in your environment
-[ -z "$ANDROID_KEYSTORE_FILE" ] && ANDROID_KEYSTORE_FILE=~/.android/debug.keystore
+[ -z "$ANDROID_KEYSTORE_FILE" ] && ANDROID_KEYSTORE_FILE=$HOME/.android/debug.keystore
 [ -z "$ANDROID_KEYSTORE_ALIAS" ] && ANDROID_KEYSTORE_ALIAS=androiddebugkey
-PASS=
+PASS="--ks-pass pass:android"
 [ -n "$ANDROID_KEYSTORE_PASS" ] && PASS="--ks-pass env:ANDROID_KEYSTORE_PASS"
 [ -n "$ANDROID_KEYSTORE_PASS_FILE" ] && PASS="--ks-pass file:$ANDROID_KEYSTORE_PASS_FILE"
 
@@ -19,7 +19,7 @@ cp -f app-release.apk Signed.apk
 rm -f ../../../../../../$APPNAME-$APPVER.apk
 zipalign 4 Signed.apk ../../../../../../$APPNAME-$APPVER.apk
 rm -f Signed.apk
-echo Using keystore $ANDROID_KEYSTORE_FILE and alias $ANDROID_KEYSTORE_ALIAS
+echo Using keystore $ANDROID_KEYSTORE_FILE
 stty -echo
 apksigner sign --ks $ANDROID_KEYSTORE_FILE --ks-key-alias $ANDROID_KEYSTORE_ALIAS $PASS ../../../../../../$APPNAME-$APPVER.apk || exit 1
 stty echo
--- signBundle.sh
+++ signBundle.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # Set path to your Android keystore and your keystore alias here, or put them in your environment
-PASS=
+[ -z "$ANDROID_UPLOAD_KEYSTORE_FILE" ] && ANDROID_UPLOAD_KEYSTORE_FILE=$HOME/.android/debug.keystore
+[ -z "$ANDROID_UPLOAD_KEYSTORE_ALIAS" ] && ANDROID_UPLOAD_KEYSTORE_ALIAS=androiddebugkey
+PASS="-storepass android"
 [ -n "$ANDROID_UPLOAD_KEYSTORE_PASS" ] && PASS="-storepass:env ANDROID_UPLOAD_KEYSTORE_PASS"
 [ -n "$ANDROID_UPLOAD_KEYSTORE_PASS_FILE" ] && PASS="-storepass:file $ANDROID_UPLOAD_KEYSTORE_PASS_FILE"
 
@@ -21,7 +23,7 @@ cd app/build/outputs/bundle/releaseWithDebugInfo || exit 1
 # Remove old certificate
 cp -f app-releaseWithDebugInfo.aab ../../../../../../$APPNAME-$APPVER.aab || exit 1
 # Sign with the new certificate
-echo Using keystore $ANDROID_UPLOAD_KEYSTORE_FILE and alias $ANDROID_UPLOAD_KEYSTORE_ALIAS
+echo Using keystore $ANDROID_UPLOAD_KEYSTORE_FILE
 stty -echo
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $ANDROID_UPLOAD_KEYSTORE_FILE $PASS ../../../../../../$APPNAME-$APPVER.aab $ANDROID_UPLOAD_KEYSTORE_ALIAS || exit 1
 stty echo
