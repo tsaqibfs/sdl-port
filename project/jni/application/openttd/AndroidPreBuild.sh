84733630128299708e256451bd07638ca72536a6
Miguel Horta <mmiguelhorta@gmail.com>
2022-04-26 18:21:32 +0100

    Allow clang-7 build for built-tools

--- project/jni/application/openttd/AndroidPreBuild.sh
+++ project/jni/application/openttd/AndroidPreBuild.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
 mkdir -p build-tools
-[ -e build-tools/Makefile ] || ${CMAKE_BIN_LOC}cmake -DOPTION_TOOLS_ONLY=ON -B build-tools src
+if [ ! -e build-tools/Makefile ]; then
+	if $(c++ --version | grep -qi clang); then
+		${CMAKE_BIN_LOC}cmake -DCMAKE_CXX_FLAGS=-stdlib=libc++ -DOPTION_TOOLS_ONLY=ON -B build-tools src
+	else
+		${CMAKE_BIN_LOC}cmake -DOPTION_TOOLS_ONLY=ON -B build-tools src
+	fi
+fi
+
 make -C build-tools -j8 VERBOSE=1 || exit 1
