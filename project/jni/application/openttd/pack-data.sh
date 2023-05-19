1f301e2d7cba32eac5cf574e7b475b1f4ca7bb0d
pelya <x.pelya.x@gmail.com>
2023-05-19 11:57:41 +0300

    OpenTTD: faster builds

--- project/jni/application/openttd/pack-data.sh
+++ project/jni/application/openttd/pack-data.sh
@@ -7,33 +7,41 @@ ARCH=$1
 ANDROID_DATA_FULLPATH=$(realpath ./AndroidData/)
 
 # Base game data
-pushd ./data
-rm -f ${ANDROID_DATA_FULLPATH}/openttd-data-*.zip.xz ${ANDROID_DATA_FULLPATH}/openttd-data-*.zip
+[ -e ${ANDROID_DATA_FULLPATH}/openttd-data-$VER.zip.xz ] && [ -n "$NO_REBUILD_DATA" ] || {
+	pushd ./data
+	rm -f ${ANDROID_DATA_FULLPATH}/openttd-data-*.zip.xz ${ANDROID_DATA_FULLPATH}/openttd-data-*.zip
 
-pushd ./baseset
-	cp ../../data-plat-indp/opengfx*.tar .
-	cp ../../data-plat-indp/opensfx*.tar .
-	cp -r ../../data-plat-indp/openmsx*/ .
-popd
+	pushd ./baseset
+		cp ../../data-plat-indp/opengfx*.tar .
+		cp ../../data-plat-indp/opensfx*.tar .
+		cp -r ../../data-plat-indp/openmsx*/ .
+	popd
 
-zip -0 -r ${ANDROID_DATA_FULLPATH}/openttd-data-$VER.zip ./ && xz -8 ${ANDROID_DATA_FULLPATH}/openttd-data-$VER.zip
-popd
+	zip -0 -r ${ANDROID_DATA_FULLPATH}/openttd-data-$VER.zip ./ && xz -8 ${ANDROID_DATA_FULLPATH}/openttd-data-$VER.zip
+	popd
+}
 
 # Timidity
-pushd ../../timidity/samples/
-rm -f ${ANDROID_DATA_FULLPATH}/timidity.zip.xz ${ANDROID_DATA_FULLPATH}/timidity.zip
-cp ./timidity.zip ${ANDROID_DATA_FULLPATH}/timidity.zip && xz -8 ${ANDROID_DATA_FULLPATH}/timidity.zip
-popd
+[ -e ${ANDROID_DATA_FULLPATH}/timidity.zip.xz ] && [ -n "$NO_REBUILD_DATA" ] || {
+	pushd ../../timidity/samples/
+	rm -f ${ANDROID_DATA_FULLPATH}/timidity.zip.xz ${ANDROID_DATA_FULLPATH}/timidity.zip
+	cp ./timidity.zip ${ANDROID_DATA_FULLPATH}/timidity.zip && xz -8 ${ANDROID_DATA_FULLPATH}/timidity.zip
+	popd
+}
 
 # ICU
 # TODO handle versioning. Use Makefile var
-pushd ../../iconv/src/$ARCH/
-rm -f ${ANDROID_DATA_FULLPATH}/icudt62l.zip.xz ${ANDROID_DATA_FULLPATH}/icudt62l.zip
-zip -0 ${ANDROID_DATA_FULLPATH}/icudt62l.zip share/icu/62.1/icudt62l.dat && xz -8 ${ANDROID_DATA_FULLPATH}/icudt62l.zip
-popd
+[ -e ${ANDROID_DATA_FULLPATH}/icudt62l.zip.xz ] && [ -n "$NO_REBUILD_DATA" ] || {
+	pushd ../../iconv/src/$ARCH/
+	rm -f ${ANDROID_DATA_FULLPATH}/icudt62l.zip.xz ${ANDROID_DATA_FULLPATH}/icudt62l.zip
+	zip -0 ${ANDROID_DATA_FULLPATH}/icudt62l.zip share/icu/62.1/icudt62l.dat && xz -8 ${ANDROID_DATA_FULLPATH}/icudt62l.zip
+	popd
+}
 
 # Fonts
-pushd ../../freetype/
-rm -f ${ANDROID_DATA_FULLPATH}/openttd-fonts.zip.xz ${ANDROID_DATA_FULLPATH}/openttd-fonts.zip
-zip -0 -r ${ANDROID_DATA_FULLPATH}/openttd-fonts.zip ./fonts/ && xz -8 ${ANDROID_DATA_FULLPATH}/openttd-fonts.zip
-popd
+[ -e ${ANDROID_DATA_FULLPATH}/openttd-fonts.zip.xz ] && [ -n "$NO_REBUILD_DATA" ] || {
+	pushd ../../freetype/
+	rm -f ${ANDROID_DATA_FULLPATH}/openttd-fonts.zip.xz ${ANDROID_DATA_FULLPATH}/openttd-fonts.zip
+	zip -0 -r ${ANDROID_DATA_FULLPATH}/openttd-fonts.zip ./fonts/ && xz -8 ${ANDROID_DATA_FULLPATH}/openttd-fonts.zip
+	popd
+}
