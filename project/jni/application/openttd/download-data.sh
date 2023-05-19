940b04c8ce2e2964ccdae53a86287877653eaf52
pelya <x.pelya.x@gmail.com>
2023-05-19 10:14:20 +0300

    OpenTTD: override compiler optimization flags so compilation takes less than two hours on my 15 years old laptop

--- project/jni/application/openttd/download-data.sh
+++ project/jni/application/openttd/download-data.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
 set -e
-set -x
+
+# Base game data
+mkdir -p ./data-plat-indp
+pushd ./data-plat-indp
+cmp .ottdrev ../src/.ottdrev && {
+	echo "Version did not change - no need to download data files"
+	exit
+}
 
 GFX_VERSION=7.1
 SFX_VERSION=1.0.3
@@ -13,10 +20,6 @@ SFX_VERSION=$(curl --fail https://cdn.openttd.org/opensfx-releases/latest.yaml |
 MSX_VERSION=$(curl --fail https://cdn.openttd.org/openmsx-releases/latest.yaml | grep -Po "version: \K[0-9.]+")
 
 
-# Base game data
-mkdir -p ./data-plat-indp
-pushd ./data-plat-indp
-
 if ! [ -e "./opengfx-${GFX_VERSION}.tar" ]; then
 	curl --fail https://cdn.openttd.org/opengfx-releases/${GFX_VERSION}/opengfx-${GFX_VERSION}-all.zip | jar xv
 fi
@@ -27,3 +30,5 @@ if ! [ -d "./openmsx-${MSX_VERSION}/" ]; then
 	curl --fail https://cdn.openttd.org/openmsx-releases/${MSX_VERSION}/openmsx-${MSX_VERSION}-all.zip | jar xv
 	tar xvf ./openmsx-${MSX_VERSION}.tar && rm ./openmsx-${MSX_VERSION}.tar
 fi
+
+cp -f ../src/.ottdrev ./
